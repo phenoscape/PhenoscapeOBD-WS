bb883d64fc1ebec6310bfb375f67b28fe7444ddd
Cartik Kothari <cartik.kothari@duke.edu>
2008-12-20 00:18:16 +0000

    Added new resource mapping for Gene Search for anatomy

--- src/org/obd/ws/application/OBDApplication.java
+++ src/org/obd/ws/application/OBDApplication.java
@@ -40,7 +40,9 @@ public class OBDApplication extends Application {
         router.attach("/term/search", org.obd.ws.resources.AutoCompleteResource.class);
         router.attach("/term/{termID}", org.obd.ws.resources.TermResource.class);
         router.attach("/phenotypes/summary/anatomy/{termID}", org.obd.ws.resources.AnatomyResource.class);
-
+        router.attach("/phenotypes/anatomy/{termID}/taxa/{patoID}", org.obd.ws.resources.TaxaForAnatomyResource.class);
+        router.attach("/phenotypes/anatomy/{termID}/genes/{patoID}", org.obd.ws.resources.GenesForAnatomyResource.class);
+        
         return router;
     }
 
