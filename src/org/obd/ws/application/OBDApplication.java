751caaa0bcc0b6dac3f7a20885c08b680388c29c
Cartik Kothari <cartik.kothari@duke.edu>
2009-01-13 19:40:14 +0000

    Fixed unused imports

--- src/org/obd/ws/application/OBDApplication.java
+++ src/org/obd/ws/application/OBDApplication.java
@@ -1,12 +1,6 @@
 package org.obd.ws.application;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.Properties;
 
