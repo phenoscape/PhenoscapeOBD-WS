e5cca243c8e46fc2c6347ff6eec95c705293a54b
Jim Balhoff <balhoff@nescent.org>
2009-05-01 14:04:05 +0000

    Moved tests into test source folder.

--- test/org/obd/ws/application/test/OBDApplicationTest.java
+++ test/org/obd/ws/application/test/OBDApplicationTest.java
@@ -3,6 +3,8 @@ package org.obd.ws.application.test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
 
 import org.junit.Test;
 import org.obd.query.Shard;
@@ -15,28 +17,23 @@ import org.restlet.resource.Representation;
 public class OBDApplicationTest {
 
 	@Test
-	public void testTermSearch(){
-		try {
-			File connParamFile = new File("testfiles/connectionParameters");
-			BufferedReader br = new BufferedReader(
-					new FileReader(connParamFile));
-			String[] connParams = new String[3];
-			String param;
-			int j = 0;
-			while ((param = br.readLine()) != null) {
-				connParams[j++] = param;
-			}
+	public void testTermSearch() throws IOException, SQLException, ClassNotFoundException{
+	    File connParamFile = new File("testfiles/connectionParameters");
+	    BufferedReader br = new BufferedReader(new FileReader(connParamFile));
+	    String[] connParams = new String[3];
+	    String param;
+	    int j = 0;
+	    while ((param = br.readLine()) != null) {
+	        connParams[j++] = param;
+	    }
 
-			Shard obdsql = new OBDSQLShard();
-			((AbstractSQLShard) obdsql).connect(connParams[0], connParams[1],
-					connParams[2]);
-			TermResource tr = new TermResource(obdsql, "ZFA:0000107");
-			Representation rep2 = tr.getRepresentation(tr.getVariants().get(0));
-			AutoCompleteResource acr = new AutoCompleteResource(obdsql, "basihyal", new String[]{"true", "true", "true", "TTO:TAO:COLLECTION"});
-			Representation rep = acr.getRepresentation(acr.getVariants().get(0));
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+	    Shard obdsql = new OBDSQLShard();
+	    ((AbstractSQLShard) obdsql).connect(connParams[0], connParams[1],
+	            connParams[2]);
+	    TermResource tr = new TermResource(obdsql, "ZFA:0000107");
+	    Representation rep2 = tr.getRepresentation(tr.getVariants().get(0));
+	    AutoCompleteResource acr = new AutoCompleteResource(obdsql, "basihyal", new String[]{"true", "true", "true", "TTO:TAO:COLLECTION"});
+	    Representation rep = acr.getRepresentation(acr.getVariants().get(0));
 	}
+	
 }
