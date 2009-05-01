604d907a0106b9d1d01e3842cfec86da7bb2afa2
Hilmar Lapp <hlapp@nescent.org>
2009-05-01 14:37:06 +0000

    Added missing documentation comments for code review. Sort of in vain, because it turns out we can get rid of this entirely.

--- src/org/nescent/informatics/SQLGenerator.java
+++ src/org/nescent/informatics/SQLGenerator.java
@@ -6,42 +6,79 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * FIXME Class comment missing.
+ */
 public class SQLGenerator {
 	
 	private List<LinkStatement> linkStatementList;
 	private String sqlStr;
 	
+        /**
+         * FIXME Constructor and parameter documentation missing.
+         */
 	public SQLGenerator(){
 		linkStatementList = new ArrayList<LinkStatement>();
-		sqlStr = null;
+		sqlStr = null; // not needed - it's Java ...
 	}
 	
+        /**
+         * FIXME Constructor and parameter documentation missing.
+         */
 	public SQLGenerator(List<LinkStatement> lss){
+            // FIXME doesn't chain the constructor 
+
+            // FIXME any reason why aren't calling our own setter
+            // here, given that there is one?
+
 		linkStatementList = lss;
 	}
 	
+        /**
+         * FIXME Constructor and parameter documentation missing.
+         */
 	public SQLGenerator(LinkStatement ls){
 		this();
+                // FIXME should be using our own getter - otherwise we
+                // are bypassing derived classes that override it. If
+                // you don't want that, make the getter final.
 		linkStatementList.add(ls);
 	}
 	
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public List<LinkStatement> getLinkStatementList() {
 		return linkStatementList;
 	}
 	
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public void setLinkStatementList(List<LinkStatement> linkStatementList) {
 		this.linkStatementList = linkStatementList;
 	}
 	
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public String getSqlStr() {
 		return sqlStr;
 	}
 
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public void setSqlStr(String sqlStr) {
 		this.sqlStr = sqlStr;
 	}
 	
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public List<String> constructPaths(List<String> paths, List<LinkStatement> lsList){
+
+            // FIXME what if lsList is null?
 		if(lsList.size() == 0){
 			return paths;
 		}
@@ -94,6 +131,9 @@ public class SQLGenerator {
 		return null;
 	}
 	
+        /**
+         * FIXME Method and parameter documentation missing.
+         */
 	public String constructSQLQuery(){
 		String sql = "";
 		for(LinkStatement ls : linkStatementList){
