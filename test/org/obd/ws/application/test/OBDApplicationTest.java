ac025831b199f46a15e938b44248b0da76223296
Hilmar Lapp <hlapp@nescent.org>
2009-05-02 15:45:13 +0000

    Committing test file before we move it.

--- test/org/obd/ws/application/test/OBDApplicationTest.java
+++ test/org/obd/ws/application/test/OBDApplicationTest.java
@@ -13,11 +13,14 @@ import org.obd.query.impl.OBDSQLShard;
 import org.obd.ws.resources.AutoCompleteResource;
 import org.obd.ws.resources.TermResource;
 import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
 
 public class OBDApplicationTest {
 
 	@Test
-	public void testTermSearch() throws IOException, SQLException, ClassNotFoundException{
+	public void testTermSearch() 
+            throws ResourceException, IOException, SQLException, ClassNotFoundException {
+
 	    File connParamFile = new File("testfiles/connectionParameters");
 	    BufferedReader br = new BufferedReader(new FileReader(connParamFile));
 	    String[] connParams = new String[3];
@@ -31,9 +34,9 @@ public class OBDApplicationTest {
 	    ((AbstractSQLShard) obdsql).connect(connParams[0], connParams[1],
 	            connParams[2]);
 	    TermResource tr = new TermResource(obdsql, "ZFA:0000107");
-	    Representation rep2 = tr.getRepresentation(tr.getVariants().get(0));
+	    Representation rep2 = tr.represent(tr.getVariants().get(0));
 	    AutoCompleteResource acr = new AutoCompleteResource(obdsql, "basihyal", new String[]{"true", "true", "true", "TTO:TAO:COLLECTION"});
-	    Representation rep = acr.getRepresentation(acr.getVariants().get(0));
+	    Representation rep = acr.represent(acr.getVariants().get(0));
 	}
 	
 }
