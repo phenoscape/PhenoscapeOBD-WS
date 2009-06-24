6bce688e52caaa471b57b0572675352439d2550b
Cartik Kothari <cartik.kothari@duke.edu>
2009-06-24 19:53:05 +0000

    Changed query to find results for entities that are leaves in the TAO

--- src/org/obd/ws/util/Queries.java
+++ src/org/obd/ws/util/Queries.java
@@ -118,7 +118,24 @@ public class Queries {
 		"	subtaxon_link.predicate_id = ___is_a) " +
 		"JOIN phenotype_by_entity_character AS p1 ON (p1.entity_nid = subtaxon_link.node_id) " +
 		"WHERE " +
-		"search_node.uid = ?";
+		"search_node.uid = ? " +
+		"UNION " +
+		"SELECT " +
+		"p1.phenotype_uid AS phenotype_uid, " +
+		"p1.subject_uid AS subject_uid, " +
+		"p1.subject_label AS subject_Label, " +
+		"p1.quality_uid AS quality_uid, " +
+		"p1.quality_label AS quality_label, " +
+		"p1.character_uid AS character_uid, " +
+		"p1.character_label AS character_label, " +
+		"p1.entity_uid AS entity_uid, " +
+		"p1.entity_label AS entity_label, " +
+		"p1.reif_id AS reif_id, " +
+		"p1.count AS count " +
+		"FROM " +
+		"phenotype_by_entity_character AS p1 " +
+		"WHERE " +
+		"p1.entity_uid = ?";
 	
 	/**
 	 * @INPUT: A gene (G)
