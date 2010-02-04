e246f23ef43b35b14f20ccc30a0e12a5b9c43918
Jim Balhoff <balhoff@nescent.org>
2010-02-04 03:33:43 +0000

    Beginning of treemap data resource.

---
+++ src/org/obd/ws/resources/AbstractOBDResource.java
@@ -0,0 +1,56 @@
+package org.obd.ws.resources;
+
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+import org.obd.query.impl.OBDSQLShard;
+import org.obd.ws.application.OBDApplication;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Resource;
+
+public class AbstractOBDResource extends Resource {
+
+    private static final String DRIVER_NAME = "jdbc:postgresql://";
+    private OBDSQLShard shard;
+
+    public AbstractOBDResource(Context context, Request request, Response response) {
+        super(context, request, response);
+        try {
+            this.shard = new OBDSQLShard();
+        } catch (SQLException e) {
+            log().fatal("Failed to create shard", e);
+        } catch (ClassNotFoundException e) {
+            log().fatal("Failed to create shard", e);
+        }
+    }
+
+    protected OBDSQLShard getShard() {
+        return this.shard;
+    }
+
+    /**
+     * This method reads in db connection parameters from app context before connecting
+     * the Shard to the database
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    protected void connectShardToDatabase() throws SQLException, ClassNotFoundException{
+        String dbName = (String)this.getContext().getAttributes().get(OBDApplication.SELECTED_DATABASE_NAME_STRING);
+        String dbHost = (String)this.getContext().getAttributes().get(OBDApplication.DB_HOST_NAME_STRING);
+        String uid = (String)this.getContext().getAttributes().get(OBDApplication.UID_STRING);
+        String pwd = (String)this.getContext().getAttributes().get(OBDApplication.PWD_STRING);
+        String dbConnString = DRIVER_NAME + dbHost + "/" + dbName;
+        this.shard.connect(dbConnString, uid, pwd);
+    }
+
+    protected void disconnectShardFromDatabase() {
+        this.shard.disconnect();
+    }
+
+    protected Logger log() {
+        return Logger.getLogger(this.getClass());
+    }
+
+}
---
+++ src/org/obd/ws/resources/SquarifiedTaxonMapResource.java
@@ -0,0 +1,64 @@
+package org.obd.ws.resources;
+
+import java.sql.SQLException;
+
+import org.json.JSONObject;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+
+public class SquarifiedTaxonMapResource extends AbstractOBDResource {
+    
+    private final String taxonID;
+
+    public SquarifiedTaxonMapResource(Context context, Request request, Response response) {
+        super(context, request, response);
+        this.getVariants().add(new Variant(MediaType.APPLICATION_JSON));
+        this.taxonID = Reference.decode((String)(request.getAttributes().get("taxonID")));
+    }
+
+    @Override
+    public Representation represent(Variant variant) throws ResourceException {
+        try {
+            this.connectShardToDatabase();
+            final JSONObject json = new JSONObject();
+            // the value for the "$area" key should be the 
+            // result of getCountOfAnnotationsWithinTaxon(), instead of 42
+            //
+            // return children only one level deep - all children have an empty
+            // children list
+            
+//            {id: "TTO:151", name: "Halecostomi", data:{"$area": 42}, children: [
+//{"id":"TTO:10000323","name":"Pachycormidae", data:{"$area": 42}, children: []},
+//{"id":"TTO:1220", "name":"Amiiformes", data:{"$area": 42}, children: []},
+//{"id":"TTO:10000305","name":"Aspidorhynchiformes", data:{"$area": 42}, children: []},
+//{"id":"TTO:201","name":"Teleostei", data:{"$area": 42}, children: []},
+// {"id":"TTO:10000290","name":"Parasemionotidae", data:{"$area": 42}, children: []}
+//]}
+            
+            
+
+            return new JsonRepresentation(json);
+        } catch (SQLException sqle) {
+            getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "[SQL EXCEPTION] Something broke server side. Consult server logs");
+        } catch (ClassNotFoundException e) {
+            getResponse().setStatus(Status.SERVER_ERROR_INTERNAL, "[CLASS NOT FOUND EXCEPTION] Something broke server side. Consult server logs");
+        } finally {
+            disconnectShardFromDatabase();
+        }
+        return null;
+    }
+    
+    private int getCountOfAnnotationsWithinTaxon(String taxonUID) {
+        //TODO get the real number
+        return 42;
+    }
+
+}
