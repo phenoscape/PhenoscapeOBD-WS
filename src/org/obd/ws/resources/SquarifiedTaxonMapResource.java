de4ef9049e29ef73775f6c08284032e2d36c0113
Jim Balhoff <balhoff@nescent.org>
2010-02-04 20:58:16 +0000

    Added more specification to tree map service.

--- src/org/obd/ws/resources/SquarifiedTaxonMapResource.java
+++ src/org/obd/ws/resources/SquarifiedTaxonMapResource.java
@@ -31,16 +31,18 @@ public class SquarifiedTaxonMapResource extends AbstractOBDResource {
             final JSONObject json = new JSONObject();
             // the value for the "$area" key should be the 
             // result of getCountOfAnnotationsWithinTaxon(), instead of 42
+            // the value for the "species" key should be the
+            // number of taxa of rank species within the given taxon
             //
             // return children only one level deep - all children have an empty
             // children list
             
-//            {id: "TTO:151", name: "Halecostomi", data:{"$area": 42}, children: [
-//{"id":"TTO:10000323","name":"Pachycormidae", data:{"$area": 42}, children: []},
-//{"id":"TTO:1220", "name":"Amiiformes", data:{"$area": 42}, children: []},
-//{"id":"TTO:10000305","name":"Aspidorhynchiformes", data:{"$area": 42}, children: []},
-//{"id":"TTO:201","name":"Teleostei", data:{"$area": 42}, children: []},
-// {"id":"TTO:10000290","name":"Parasemionotidae", data:{"$area": 42}, children: []}
+//            {id: "TTO:151", name: "Halecostomi", data:{"$area": 42, species: 1234}, children: [
+//{"id":"TTO:10000323","name":"Pachycormidae", data:{"$area": 42, species: 1234}, children: []},
+//{"id":"TTO:1220", "name":"Amiiformes", data:{"$area": 42, species: 1234}, children: []},
+//{"id":"TTO:10000305","name":"Aspidorhynchiformes", data:{"$area": 42, species: 1234}, children: []},
+//{"id":"TTO:201","name":"Teleostei", data:{"$area": 42, species: 1234}, children: []},
+// {"id":"TTO:10000290","name":"Parasemionotidae", data:{"$area": 42, species: 1234}, children: []}
 //]}
             
             
