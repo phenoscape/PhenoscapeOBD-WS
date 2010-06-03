755a18d670daa80d68b8309eeda44005ec540b4c
Jim Balhoff <balhoff@nescent.org>
2010-06-03 17:42:15 +0000

    Docs.

--- src/org/phenoscape/ws/representation/StreamableJSONRepresentation.java
+++ src/org/phenoscape/ws/representation/StreamableJSONRepresentation.java
@@ -8,20 +8,49 @@ import org.json.JSONObject;
 import org.restlet.data.CharacterSet;
 import org.restlet.data.MediaType;
 
+/**
+ * A Representation which can be used for very large datasets without requiring that the full dataset is 
+ * in memory all at once.  The provided Iterator can be implemented to retrieve data items on demand. The 
+ * data items are represented as a root-level JSON array by default, but the array can be wrapped in an outer 
+ * JSON object if a key is provided.   
+ * @author Jim Balhoff
+ */
 public class StreamableJSONRepresentation extends StreamableTextRepresentation {
 
     private final String key;
     private final JSONObject otherValues;
     private static final String SEPARATOR = "," + System.getProperty("line.separator"); 
 
+    /**
+     * Create a Representation for the JSON objects provided by the given Iterator.
+     * @param items The JSON objects to be written to the output. toString will be called on each item 
+     * as it is written to the output, which will be in the form of a JSON array.
+     */
     public StreamableJSONRepresentation(Iterator<? extends JSONObject> items) {
         this(items, null);
     }
 
+    /**
+     * Create a Representation for the JSON objects provided by the given Iterator.
+     * @param items The JSON objects to be written to the output. toString will be called on each item 
+     * as it is written to the output.
+     * @param key If a key is provided, the output will be a JSON object with the JSON array 
+     * representing the data items included as the value for this key.
+     */
     public StreamableJSONRepresentation(Iterator<? extends JSONObject> items, String key) {
         this(items, key, new JSONObject());
     }
 
+    /**
+     * Create a Representation for the JSON objects provided by the given Iterator.
+     * @param items The JSON objects to be written to the output. toString will be called on each item 
+     * as it is written to the output.
+     * @param key If a key is provided, the output will be a JSON object with the JSON array 
+     * representing the data items included as the value for this key.
+     * @param otherValues A JSON object containing other keys and values which should be included in the output 
+     * JSON object. These keys and values will be written before the key containing the JSON array of data items. 
+     * Any data in this JSON object will not benefit from the streaming used for the iterated data items. 
+     */
     public StreamableJSONRepresentation(Iterator<? extends JSONObject> items, String key, JSONObject otherValues) {
         super(items, MediaType.APPLICATION_JSON);
         this.key = key;
@@ -44,6 +73,9 @@ public class StreamableJSONRepresentation extends StreamableTextRepresentation {
         if (this.key != null) { writer.write("}"); }
     }
 
+    /**
+     * Returns a separator appropriate for JSON array syntax - a comma and newline by default. 
+     */
     @Override
     protected String getItemSeparator() {
         return SEPARATOR;
--- src/org/phenoscape/ws/representation/StreamableTextRepresentation.java
+++ src/org/phenoscape/ws/representation/StreamableTextRepresentation.java
@@ -7,15 +7,31 @@ import java.util.Iterator;
 import org.restlet.data.MediaType;
 import org.restlet.representation.WriterRepresentation;
 
+/**
+ * A Representation which can be used for very large datasets without requiring that the full dataset is 
+ * in memory as a String all at once.  The provided Iterator can be implemented to retrieve data items on demand.  
+ * @author Jim Balhoff
+ */
 public class StreamableTextRepresentation extends WriterRepresentation {
 
     private final Iterator<? extends Object> items;
     private static final String SEPARATOR = System.getProperty("line.separator"); 
 
+    /**
+     * Create a Representation for the items provided by the given Iterator.
+     * @param items The items to be written to the output. toString will be called on each item 
+     * as it is written to the output.  Items will be separated by a newline character by default.
+     */
     public StreamableTextRepresentation(Iterator<?> items) {
         this(items, MediaType.TEXT_PLAIN);
     }
 
+    /**
+     * Create a Representation for the items provided by the given Iterator.
+     * @param items The items to be written to the output. toString will be called on each item 
+     * as it is written to the output.  Items will be separated by a newline character by default.
+     * @param mediaType The MediaType to be used for the output.
+     */
     public StreamableTextRepresentation(Iterator<?> items, MediaType mediaType) {
         super(mediaType);
         this.items = items;
@@ -31,6 +47,10 @@ public class StreamableTextRepresentation extends WriterRepresentation {
         }
     }
 
+    /**
+     * Get the String which will be inserted between each output item. Returns 
+     * System.getProperty("line.separator") by default.
+     */
     protected String getItemSeparator() {
         return SEPARATOR;
     }
