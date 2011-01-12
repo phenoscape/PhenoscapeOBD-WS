b7764314109c7bf392837e29e523e868b144d2f9
Jim Balhoff <balhoff@nescent.org>
2011-01-12 16:53:14 -0500

    Fixed bug preventing species counts for unranked taxa.

--- src/org/phenoscape/obd/query/SpeciesCountQueryBuilder.java
+++ src/org/phenoscape/obd/query/SpeciesCountQueryBuilder.java
@@ -25,7 +25,7 @@ public class SpeciesCountQueryBuilder extends QueryBuilder {
         query.append("SELECT count(*) as species_count FROM taxon parent ");
         query.append(String.format("JOIN link is_a_link ON (is_a_link.predicate_id = %s AND is_a_link.object_id = parent.node_id) ", this.node(OBO.IS_A)));
         query.append(String.format("JOIN taxon child ON (child.node_id = is_a_link.node_id AND child.rank_uid = '%s') ", TAXRANK.SPECIES));
-        query.append(String.format("WHERE parent.uid = ? AND parent.rank_uid != '%s' ", TAXRANK.SPECIES));
+        query.append(String.format("WHERE parent.uid = ? AND (parent.rank_uid != '%s' OR parent.rank_uid IS NULL) ", TAXRANK.SPECIES));
         return query.toString();
     }
 
